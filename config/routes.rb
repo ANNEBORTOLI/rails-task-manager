82d01c540d736a0169ed4b4b8b36a5934c2c9094
Anne Bortoli <annebortoli@gmail.com>
2023-05-23 16:35:55 -0300

    basic crud done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # List all tasks
+  get '/tasks', to: 'tasks#index'
+
+  # 2. receive the form submission
+  post '/tasks', to: 'tasks#create'
+
+  # Create a task
+  # 1. see the form
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+
+  # Update a task
+  # 1. see the form
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # See one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
